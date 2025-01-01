81203bbbd1f8f293490b20f8ec41af660cb76c0f
Yohannes Mebrahtu <johannes.m.tekle@gmail.com>
2025-01-01 21:30:26 +0000

    add input mehtod

--- run.py
+++ run.py
@@ -1,3 +1,20 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+
+
+if __name__ == "__main__":
+    render(10,10)
+
+    inp = input("Where do you want to shoot?\n")
+    xstr,ystr = inp.split(",")
+    x = int(xstr)
+    y = int(ystr)
+    print(x)
+    print(y)
+    print(x+y)
+
+
+
+
