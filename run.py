c4a6146df8daa78fe6ea668be7faae1534657bdc
Yohannes Mebrahtu <johannes.m.tekle@gmail.com>
2025-01-01 21:59:19 +0000

    get input and render board

--- run.py
+++ run.py
@@ -2,18 +2,36 @@
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
 
+def render(board_width, board_height, shots):
+    header = "+" + "_" * board_width + "+"
+    print(header)
 
+    shots_set = set(shots)
+    for y in range(board_height):
+        row = []
+        for x in range(board_width):
+            if (x,y) in shots:
+                ch = "X"
+            else:
+                ch = " "
+            row.append(ch)
+        print("|" + "".join(row) + "|")
+
+    for i in range(board_height):
+        print("|" + " " * board_width + "|")
 
 if __name__ == "__main__":
-    render(10,10)
+    shots = []
 
-    inp = input("Where do you want to shoot?\n")
-    xstr,ystr = inp.split(",")
-    x = int(xstr)
-    y = int(ystr)
-    print(x)
-    print(y)
-    print(x+y)
+    while True:
+        inp = input("Where do you want to shoot?\n")
+        xstr,ystr = inp.split(",")
+        x = int(xstr)
+        y = int(ystr)
+
+        shots.append((x,y))
+        render(10, 10, shots)
+        
 
 
 
